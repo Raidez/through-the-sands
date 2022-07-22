05238353dad8e96de78a67592325879ff69f89af
Raidez <alexis.lucien@outlook.fr>
2022-07-22 14:35:31 +0200

    Add Hyperlog addon

---
+++ Scripts/World.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var ship = $Ship
+onready var player = $Player
+
+func _ready():
+	HyperLog.log(player).text("player_velocity>round")
+	HyperLog.log(player).text("state")
+	
+	HyperLog.log(ship).text("ship_velocity>round")
